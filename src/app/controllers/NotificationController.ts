3ed97653b51ee92b1a677ef600fdbb3088f25e63
djamel <ed_dahmane@esi.dz>
2018-06-24 18:30:17 +0200

    ajout de controlleur de notification

--- src/app/controllers/NotificationController.ts
+++ src/app/controllers/NotificationController.ts
@@ -1,21 +1,34 @@
 import { redis } from "../../config/redis";
 import { logger } from "../../config/logger";
-
+import {} from 'typescript-ioc'
 export var socket = require('socket.io')
 var backup:any
 
+
 export class NotificationController{
+  private static instance:NotificationController
+  public static  getSockets=()=>{
+    if( NotificationController.instance==null) {
+      console.log("premier instanciations")
+      NotificationController.instance=new NotificationController()
+    }
+    return NotificationController.instance
+  }
+  private static connected=socket()
+  public static getConnected=()=> NotificationController.connected
+  
   public initialiserNotificationSocket = function(server:any){
     const notif = socket(server)
-    backup=notif
+    NotificationController.connected=notif
+    console.log("lancement de serveur de notification")
     notif.on("chat message",function(client:any){
-      console.log("Test Message chat")
+     console.log("Test Message chat")
     })
     notif.on('connection',function(client:any){
       console.log('test ')
       // console.log(client)
       client.emit('ack','Bienvenue a nous !!')
-
+      console.log(client)
       client.emit('inscription','Bienvenue a nous !!')
       client.on("inscription",function(test:any){
         console.log("inscription de l'utilisateur "+test.email)
@@ -26,6 +39,7 @@ export class NotificationController{
     })
     notif.on('ack',function(msg:any){
       console.log('ack',msg)
+
     })
 
  
@@ -33,10 +47,19 @@ export class NotificationController{
     
   }
 
-  public notifierUser=function(socketId:any,message:any){
-     console.log(backup.sockets.connected)
+  public notifierUser=(socketId:any,message:any)=>{
+        console.log(NotificationController.connected)
 
     }
   
+  public notifierUserByMail=(mail:string)=>{
+      console.log("Test")
+      redis.get("ed_dahmane@esi.dz",(err,reply)=>{
+        console.log(reply)
+        this.notifierUser(reply,"Salam alikoum")
+
+      })
+      return true
+  }
 
 }
